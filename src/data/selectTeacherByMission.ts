855942a9a4d98b6413dd43df2556ef274825bc36
Fábio <fabiojordao97@yahoo.com.br>
2021-01-22 16:25:17 -0300

    Correção de bugs.

--- src/data/selectTeacherByMission.ts
+++ src/data/selectTeacherByMission.ts
@@ -1,7 +1,7 @@
 import { connection } from "../index";
 
 export const selectTeacherByMission = async (id: number): Promise<any> => {
-    const result = await connection.raw(`
+    const result = await connection.raw(`(
       SELECT name, email from teachers
       WHERE mission_id = ${id}
       )`)
--- src/endpoints/getTeacherByClass.ts
+++ src/endpoints/getTeacherByClass.ts
@@ -17,6 +17,6 @@ export const getTeacherByClass = async(req: Request, res: Response) => {
         res.status(200).send(result)
 
     } catch(error){
-
+        res.status(errorCode).send(error.message)
     }
 }
